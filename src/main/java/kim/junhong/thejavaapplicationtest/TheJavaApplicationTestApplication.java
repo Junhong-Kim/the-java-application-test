3f09dfc406bccd0d54788385874cd70c58c51c35
Junhong-Kim <jhk1005jhk@gmail.com>
2021-09-17 00:05:58 +0900

    init

---
+++ src/main/java/kim/junhong/thejavaapplicationtest/TheJavaApplicationTestApplication.java
@@ -0,0 +1,13 @@
+package kim.junhong.thejavaapplicationtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheJavaApplicationTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheJavaApplicationTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kim/junhong/thejavaapplicationtest/TheJavaApplicationTestApplicationTests.java
@@ -0,0 +1,13 @@
+package kim.junhong.thejavaapplicationtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheJavaApplicationTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
